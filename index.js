05b3288318bdc09fdcedb382861e4f057b7b9768
Nyisha Sangermano <nyishamarie.ai@gmail.com>
2026-06-22 16:01:05 -0400

    submission for question #1 and #8 on rendering so I can see it on the screen

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+// RENDERING part so I can visualize my code
+
+function render() {
+  const $app = document.querySelector("#app");
+$app.innerHTML = `
+    <h1>Freelancer Forum</h1>
+    <AverageRate></AverageRate>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Occupation</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody id="FreelancerRows"></tbody>
+    </table>
+  `;
+  $app.querySelector("AverageRate").replaceWith(AverageRate());
+  $app.querySelector("#FreelancerRows").replaceWith(FreelancerRows());
+}
+
+render();
+
+
+
+function random(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+
+// QUESTION #1
+
+function makeFreelancer() {
+    const name = random(NAMES);
+    const occupation = random(OCCUPATIONS);
+    const rate =
+        PRICE_RANGE.min +
+        Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min));
+
+    return {name, occupation, rate};
+}
